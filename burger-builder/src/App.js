b357e93594361e6bbd0622524f53c11dfb71a6b6
ketanmalik <malik.ke@husky.neu.edu>
2020-03-13 18:34:33 -0400

    burger-builder-layout

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <button className="error">Click me</button>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
